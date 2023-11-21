30e9e3a693c4db8ed2b41caa4dacd4a529f5a269
Yemny <y.feliz1296@gmail.com>
2023-11-21 16:17:47 -0500

    added conde to index.js in models

--- models/Thought.js
+++ models/Thought.js
@@ -77,10 +77,10 @@ const reactionSchema = new Schema(
     }
   );
 
-  thoughtSchema.virtual("reactionCount").get(function () {
+  thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
   });
   
-  const Thought = model("Thought", thoughtSchema);
+  const Thought = model('Thought', thoughtSchema);
   
   module.exports = Thought;
